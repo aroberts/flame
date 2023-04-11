2efb27c4efcf1cf93bc8d3035d550877e9457e66
Andrew Roberts <adroberts@gmail.com>
2023-04-10 23:28:34 -0400

    more tests, looks good for now

--- test/visibility.test.js
+++ test/visibility.test.js
@@ -12,3 +12,15 @@ test('explicit user deny, on list', () => {
 test('explicit user deny, not on list', () => {
   expect(visibility.canViewApp({denyUsers: "joe,sam"}, "fred", [])).toBe(true);
 });
+test('explicit user allow and deny ', () => {
+  expect(visibility.canViewApp({allowUsers: "joe", denyUsers: "joe,sam"}, "joe", [])).toBe(true);
+});
+test('deny group with allow user, not in list', () => {
+  expect(visibility.canViewApp({allowUsers: "joe", denyGroups: "people"}, "fred", [])).toBe(false);
+});
+test('deny group with allow user, user in list', () => {
+  expect(visibility.canViewApp({allowUsers: "joe", denyGroups: "people"}, "joe", [])).toBe(true);
+});
+test('deny group with allow user, user and group in list', () => {
+  expect(visibility.canViewApp({allowUsers: "joe", denyGroups: "people"}, "joe", ["people"])).toBe(true);
+});
--- utils/visibility.js
+++ utils/visibility.js
@@ -17,19 +17,14 @@ const canViewApp = (app, user, groups) => {
   if (!hasUserAcl && !hasGroupAcl) {
     return true;
   } else {
-    console.log("computing user")
     const cvUser = canView(allowUsers, denyUsers, [user]);
-    console.log("computing group")
     const cvGroup = canView(allowGroups, denyGroups, groups);
 
     if (cvUser != UNKNOWN) {
-      console.log("known state for user")
       return cvUser == ALLOW;
     } else if (cvGroup != UNKNOWN) {
-      console.log("known state for group")
       return cvGroup == ALLOW;
     } else {
-      console.log("default")
       return true;
     }
   }
@@ -38,23 +33,18 @@ const canViewApp = (app, user, groups) => {
 const canView = (allow, deny, inputs) => {
   // no allow, no deny -> indeterminite
   if (!allow.length && !deny.length) { 
-    console.log("no allow or deny")
     return UNKNOWN;
     // if no allow, then DENY if deny matches inputs, UNKNOWN otherwise
   } else if (!allow.length) {
-    console.log("deny " + deny + " intersect " + inputs + " " + intersects(deny, inputs))
     return (intersects(deny, inputs) ? DENY : UNKNOWN);
   } else {
     // if allow provided, then inputs are required to match allow list
-    console.log("allow " + allow + " intersect " + inputs + " " + intersects(allow, inputs))
     return (intersects(allow, inputs) ? ALLOW : DENY);
   }
 }
 
 // expects bs to be smaller, expects both to exist
 const intersects = (as, bs) => {
-  console.log(as)
-  console.log(bs)
   for (const b of bs) {
     if (as.includes(b)) {
       return true;
